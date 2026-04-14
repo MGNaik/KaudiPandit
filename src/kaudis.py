d282eba7a87e7a5b52a01107ddfac653d50591c6
MGNaik <meetkumarnaik@gmail.com>
2026-04-14 21:43:57 +0800

    Added a KaudiSet class

--- src/kaudis.py
+++ src/kaudis.py
@@ -2,7 +2,7 @@ from enum import Enum
 import random
 
 class KaudiState(Enum):
-    UP = 'up'
+    UP = 'up' # Up is the mouth of the kaudi up, open side, flat side - which counts as 1
     DOWN = 'down'
     NOT_ROLLED = 'not_rolled'
 
@@ -17,3 +17,26 @@ class Kaudi:
     
     def reset(self):
         self.state = KaudiState.NOT_ROLLED
+
+
+class KaudiSet:
+    def __init__(self, num_kaudis=6, up_prob_list=None):
+        if up_prob_list is None:
+            up_prob_list = [0.5 for i in range(num_kaudis)]
+        self.kaudis = [Kaudi(up_prob=up_prob_list[i]) for i in range(num_kaudis)]
+        self.KaudiSetValue = 0
+        
+    def roll(self):
+        self.KaudiSetValue = 0
+        for kaudi in self.kaudis:
+            kaudi.roll()
+            if (kaudi.state == KaudiState.UP):
+                self.KaudiSetValue += 1
+        if self.KaudiSetValue == 0:
+            self.KaudiSetValue = 12
+        for kaudi in self.kaudis:
+            kaudi.reset()
+
+
+
+
