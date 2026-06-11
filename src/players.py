18b34cfe99006134485c892b79252b11b50da11a
MGNaik <meetkumarnaik@gmail.com>
2026-06-11 21:55:00 +0800

    Added path dictionary to player - remains to be tested

--- src/players.py
+++ src/players.py
@@ -1,7 +1,59 @@
 from enum import Enum
-from src.types import Position, rotate_ccw
+from src.types import Position, rotate_ccw, rotate_ccw_path
 from src.pieces import Piece
 
+
+BOTTOM_PATH = {
+    Position(6,3): [Position(6,4)],
+    Position(6,4): [Position(6,5)],
+    Position(6,5): [Position(6,6)],
+    Position(6,6): [Position(5,6)],
+    Position(5,6): [Position(4,6)],
+    Position(4,6): [Position(3,6)],
+    Position(3,6): [Position(2,6)],
+    Position(2,6): [Position(1,6)],
+    Position(1,6): [Position(0,6)],
+    Position(0,6): [Position(0,5)],
+    Position(0,5): [Position(0,4)],
+    Position(0,4): [Position(0,3)],
+    Position(0,3): [Position(0,2)],
+    Position(0,2): [Position(0,1)],
+    Position(0,1): [Position(0,0)],
+    Position(0,0): [Position(1,0)],
+    Position(1,0): [Position(2,0)],
+    Position(2,0): [Position(3,0)],
+    Position(3,0): [Position(4,0)],
+    Position(4,0): [Position(5,0)],
+    Position(5,0): [Position(6,0)],
+    Position(6,0): [Position(6,1)],
+    Position(6,1): [Position(6,2)],
+    Position(6,2): [Position(5,1), Position(6,3)],
+    Position(5,1): [Position(4,1)],
+    Position(4,1): [Position(3,1)],
+    Position(3,1): [Position(2,1)],
+    Position(2,1): [Position(1,1)],
+    Position(1,1): [Position(1,2)],
+    Position(1,2): [Position(1,3)],
+    Position(1,3): [Position(1,4)],
+    Position(1,4): [Position(1,5)],
+    Position(1,5): [Position(2,5)],
+    Position(2,5): [Position(3,5)],
+    Position(3,5): [Position(4,5)],
+    Position(4,5): [Position(5,5)],
+    Position(5,5): [Position(5,4)],
+    Position(5,4): [Position(5,3)],
+    Position(5,3): [Position(5,2)],
+    Position(5,2): [Position(4,2), Position(5,1)],
+    Position(4,2): [Position(3,2)],
+    Position(3,2): [Position(2,2)],
+    Position(2,2): [Position(2,3)],
+    Position(2,3): [Position(2,4)],
+    Position(2,4): [Position(3,4)],
+    Position(3,4): [Position(4,4)],
+    Position(4,4): [Position(4,3)],
+    Position(4,3): [Position(3,3), Position(4,2)],
+    Position(3,3): [],
+}
 
 
 class PlayerID(Enum):
@@ -10,20 +62,27 @@ class PlayerID(Enum):
     Top = 2
     Left = 3
 
-
 class Player:
     def __init__(self, id):
         self.id = id
         
         square = Position(6, 3)
+        path = BOTTOM_PATH
         for _ in range(self.id.value):
             square = rotate_ccw(square)
+            path = rotate_ccw_path(path)
+        
         self.starting_square = square
+        self.path = path
 
         self.pieces = []
         for i in range(6):
             self.pieces.append(Piece(self,i))
         self.piece_tuples = []
+
+        
+
+    
 
 
 
--- src/types.py
+++ src/types.py
@@ -12,3 +12,9 @@ class Square(NamedTuple):
 def rotate_ccw(pos, n=7):
     r, c = pos
     return Position(n - 1 - c, r)
+
+def rotate_ccw_path(path):
+    newpath = {}
+    for key in path:
+        newpath.update({rotate_ccw(key):[rotate_ccw(val) for val in path[key]]})
+    return newpath
